38b39c22398ba9412ad10441bae9035e245092e2
diikaanedev <diikaanedev@gmail.com>
2020-09-05 08:49:25 +0000

    feature home acceuil 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web',
+      title: 'Actu 221',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: kPrimaryColor,
-
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(),
