bed65d8baab10e99ba7cf940dcc0c332c060960b
diikaanedev <diikaanedev@gmail.com>
2020-09-09 21:47:09 +0000

    feature card-other-page

--- lib/pages/body-actualite.dart
+++ lib/pages/body-actualite.dart
@@ -1,4 +1,8 @@
+import 'package:actu221/data/articles.dart';
+import 'package:actu221/screens/home.dart';
+import 'package:actu221/widgets/card-other-page.dart';
 import 'package:actu221/widgets/input-search.dart';
+import 'package:actu221/widgets/titre-text.dart';
 import 'package:flutter/material.dart';
 
 class BodyActualite extends StatelessWidget {
@@ -6,149 +10,98 @@ class BodyActualite extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal:3 ),
+      padding: EdgeInsets.symmetric(horizontal: 3),
       child: Column(
         children: [
           Container(
             height: size.height * .13,
+            width: size.width,
             color: Colors.orange,
+            child: TitreText(
+                size: size,
+                titre: homeScreenState.screen == 1
+                    ? 'Actualité'
+                    : homeScreenState.screen == 2
+                        ? 'Politique'
+                        : homeScreenState.screen == 3
+                            ? 'Economique'
+                            : homeScreenState.screen == 4
+                                ? 'Sport'
+                                : homeScreenState.screen == 5
+                                    ? 'Société'
+                                    : homeScreenState.screen == 6
+                                        ? 'Afrique'
+                                        : homeScreenState.screen == 7
+                                            ? 'Monde'
+                                            : homeScreenState.screen == 8
+                                                ? 'Investigation'
+                                                : homeScreenState.screen == 9
+                                                    ? 'Decouverte'
+                                                    : 'Entreprenariat'),
           ),
-          Container(
+          homeScreenState.screen==1 ? Container(
             height: 85,
             width: size.width * .4,
             // color: Colors.red,
             child: Center(
               child: InputSearch(),
             ),
-          ),
+          ) :Container(),
           Padding(
-            padding: const EdgeInsets.only(left: 16.0 , right: 16.0),
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
             child: Container(
-              height: size.height * .57,
-              child: ListView(
+              height: homeScreenState.screen == 1 ? size.height * .57 : size.height * .67 ,
+              child: GridView.count(
+                crossAxisCount: 5,
                 physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.all(32.0),
+                childAspectRatio: 1.4,
                 children: [
-                  Container(
-                    height: size.height * .22,
-                    // color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                      ],
-                    ),
+                  CardOtherPage(
+                    article: articleOne,
                   ),
-                  SizedBox(
-                    height: 16,
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(article: articleTree),
+                  CardOtherPage(
+                    article: articleOne,
                   ),
-                  Container(
-                    height: size.height * .22,
-                    // color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                      ],
-                    ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(
+                    article: articleOne,
                   ),
-                  SizedBox(height: 16,),
-                  Container(
-                    height: size.height * .22,
-                    // color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                      ],
-                    ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(article: articleTree),
+                  CardOtherPage(
+                    article: articleOne,
                   ),
-                  SizedBox(height: 16,),
-                  Container(
-                    height: size.height * .22,
-                    // color: Colors.blue,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: size.width * .18,
-                          color: Colors.green,
-                        ),
-                      ],
-                    ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(
+                    article: articleOne,
                   ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(article: articleTree),
+                  CardOtherPage(
+                    article: articleOne,
+                  ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(
+                    article: articleOne,
+                  ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(article: articleTree),
+                  CardOtherPage(
+                    article: articleOne,
+                  ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(
+                    article: articleOne,
+                  ),
+                  CardOtherPage(article: articleTwo),
+                  CardOtherPage(article: articleTree),
+                  CardOtherPage(
+                    article: articleOne,
+                  ),
+                  CardOtherPage(article: articleTwo),
                 ],
               ),
             ),
--- lib/widgets/card-other-page.dart
+++ lib/widgets/card-other-page.dart
@@ -1,10 +1,73 @@
+import 'package:actu221/models/article.dart';
+import 'package:actu221/utils/constant.dart';
+import 'package:actu221/widgets/show-dialog-article.dart';
 import 'package:flutter/material.dart';
 
 class CardOtherPage extends StatelessWidget {
+  final Article article;
+  const CardOtherPage({@required this.article});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () =>  showDialogArtile(size: size, article: article, context: context),
+            child: Container(
+          width: size.width * .18,
+          height: size.height * .22,
+          decoration: BoxDecoration(
+              // color: colorPrimaire,
+              borderRadius: BorderRadius.circular(5),
+              image: DecorationImage(
+                  image: AssetImage(article.urlPhoto), fit: BoxFit.cover)),
+          child: Stack(
+            children: [
+              Container(
+                width: size.width * .18,
+                height: size.height * .22,
+              ),
+              Positioned(
+                  top: size.height * .16,
+                  left: size.width * .015,
+                  child: Container(
+                    height: size.height * 0.05,
+                    width: size.width * .15,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 248, 240, 1),
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                    child: Center(
+                      child: Text(
+                        article.titre,
+                        style: TextStyle(
+                            color: colorPrimaire,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 9),
+                      ),
+                    ),
+                  )),
+              Positioned(
+                  top: size.height * .145,
+                  left: size.width * .014,
+                  child: Container(
+                    height: size.height * 0.03,
+                    width: size.width * .07,
+                    color: colorPrimaire,
+                    child: Center(
+                      child: Text(
+                        article.tag,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
